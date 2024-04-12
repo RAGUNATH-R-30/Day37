4d54a845b9c8d3815cd959c9376e5f2fea6e6191
RAGUNATH-R-30 <62740632+RAGUNATH-R-30@users.noreply.github.com>
2024-04-12 12:43:12 +0530

    Update scriptfs.js

--- scriptfs.js
+++ scriptfs.js
@@ -1,38 +1,39 @@
-const fs = require("fs");
-const express = require("express");
-const cors = require("cors");
-const app = express();
-app.use(express.json());
+  const fs = require("fs");
+  const express = require("express");
+  const cors = require("cors");
+  const app = express();
+  app.use(express.json());
 
-app.listen(3000);
+  app.listen(3000);
+  
 
-app.get("/getalltextfiles", (req, res) => {
-  fs.readdir("D:/", (err, data) => {
-    if (err) {
-      console.error("Error reading directory:", err);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-    const allfiles = data.filter((file) => file.endsWith(".txt"));
+  app.get("/getalltextfiles", (req, res) => {
+    fs.readdir("./files", (err, data) => {
+      if (err) {
+        console.error("Error reading directory:", err);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+      const allfiles = data.filter((file) => file.endsWith(".txt"));
 
-    res.json({
-      message: "File retrived Successfully",
-      allfiles: allfiles,
+      res.json({
+        message: "File retrived Successfully",
+        allfiles: allfiles,
+      });
     });
   });
-});
 
-app.post("/createnewfile", (req, res) => {
-  let date = new Date();
-  let fileName = `${date.getFullYear()}-${(
-    date.getMonth() + 1
-  ).toString()}-${date.getDate()}-${date.getHours()}hrs-${date.getMinutes()}mins-${date.getSeconds()}secs`;
-  fs.writeFile(`D:/${fileName.toString()}.txt`, `${date}`, "utf8", (err) => {
-    if (err) {
-      console.error("Error creating file:", err);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-    res.json({
-      message: "File Created Successfully",
+  app.post("/createnewfile", (req, res) => {
+    let date = new Date();
+    let fileName = `${date.getFullYear()}-${(
+      date.getMonth() + 1
+    ).toString()}-${date.getDate()}-${date.getHours()}hrs-${date.getMinutes()}mins-${date.getSeconds()}secs`;
+    fs.writeFile(`./files/${fileName.toString()}.txt`, `${date}`, "utf8", (err) => {
+      if (err) {
+        console.error("Error creating file:", err);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+      res.json({
+        message: "File Created Successfully",
+      });
     });
   });
-});
